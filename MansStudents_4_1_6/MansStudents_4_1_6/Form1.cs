c9645828bc446d6aa6387bdf4b9cc7b8258116d4
Vladislav <111188723+deadAngalas@users.noreply.github.com>
2023-10-24 12:39:12 +0300

    added MansStudents

---
+++ MansStudents_4_1_6/MansStudents_4_1_6/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MansStudents_4_1_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student D = new Student("Deniss Olksna", "st2022123", "DVAF", 2, 6.7);
+            richTextBox1.Clear();
+            richTextBox1.AppendText("D:" + D.ToString());
+            D.Sesija(5,9,7,8,10);
+            D++;
+            richTextBox1.AppendText("\n\nD pec sesijas:" + D.ToString());
+        }
+    }
+}
---
+++ MansStudents_4_1_6/MansStudents_4_1_6/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MansStudents_4_1_6
+{
+    public class Student
+    {
+        // lauki
+        private string Name;
+        private string Nr;
+        private string Fac;
+        private int Year;
+        private double Av;
+
+        //konstruktors
+        public Student(string aName, string aNr, string aFac, int aYear, double aAv)
+        {
+            Name = aName;
+            Nr = aNr;
+            Fac = aFac;
+            Year = aYear;
+            Av = aAv;
+        }
+
+        // metodes
+        public override string ToString()
+        {
+            return "\nName: " + Name + "\nSt.apl.nr.: " + Nr + "\nFacultate: " + Fac +
+                "\nCourse: " + Year + "\nAv. Grade: " + Av;
+        }
+        public void Sesija(params int[] a)
+        {
+            double s = 0;
+            for(int k = 0; k < a.Length; k++)
+            {
+                s += a[k];
+            }
+            Av = s * 1.0 / a.Length;
+        }
+        public static Student operator ++(Student a)
+        {
+            a.Year++;
+            return a;
+        }
+    }
+
+    /*
+    Operācija ++ „pārcel” studentu nākamajā kursā (bez pārbaudēm). 
+    */
+}
